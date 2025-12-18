b9926771f506f81e5e6a94dcef2ed178089f3446
Mohamed Hassan <mohamedhassanmido123@gmail.com>
2025-12-18 17:17:53 +0200

    Update Icons.jsx

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import windIcon from '../assets/wind.png';
+import windIcon from '../assets/Wind.png';
 import humidityIcon from '../assets/humidity.png';
 import visibilityIcon from '../assets/visibility.png';
-import sunriseIcon from '../assets/sunrise.png';
-import sunsetIcon from '../assets/sunset.png';
+import sunriseIcon from '../assets/Sunrise.png';
+import sunsetIcon from '../assets/Sunset.png';
 
 const Icon = ({src,alt,className})=>{
     return(
@@ -15,4 +15,5 @@ export const WindIcon = ()=> <Icon src={windIcon} alt="wind" className='animate-
 export const HumidityIcon = ()=> <Icon src={humidityIcon} alt="humidity" className='powerful-pulse svg-hover'  />
 export const VisibilityIcon = ()=> <Icon src={visibilityIcon} alt="visibility" className='powerful-pulse svg-hover'  />
 export const SunriseIcon = ()=> <Icon src={sunriseIcon} alt="sunrise" className='powerful-pulse svg-hover'  />
+
 export const SunsetIcon = ()=> <Icon src={sunsetIcon} alt="sunset" className='powerful-pulse svg-hover'  />
